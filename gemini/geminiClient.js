fa62a8bb9cf9a2306908f03a8a72de2dbd9410ed
Smriti Jha <smritijha5487@gmail.com>
2026-02-09 18:48:43 +0530

    temp

--- gemini/geminiClient.js
+++ gemini/geminiClient.js
@@ -6,8 +6,13 @@ dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
+
 const model = genAI.getGenerativeModel({
   model: "gemini-2.5-flash",
+  generationConfig: {
+    temperature: 0.5,
+    maxOutputTokens: 200,
+  },
 });
 
 export async function askGemini(prompt) {
