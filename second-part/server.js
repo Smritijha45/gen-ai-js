16d513226b11abe6b54a11f8a9946a36f5cbec1b
Smriti Jha <smritijha5487@gmail.com>
2026-02-13 23:03:50 +0530

    port fixed

--- second-part/server.js
+++ second-part/server.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3200;
 
 app.listen(PORT, () => {
   console.log(`Embedding service running on port ${PORT}`);
 });
+//no of dimensions = 1536
